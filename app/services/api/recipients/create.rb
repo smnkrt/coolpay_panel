dade0d747a6972d83b65ca68f4ab79da0a847b11
smnkrt <szymon.krywult@netguru.pl>
2017-11-25 10:57:46 +0100

    add API::Recipients::Create class

---
+++ app/services/api/recipients/create.rb
@@ -0,0 +1,37 @@
+require 'httparty'
+
+module API::Recipients
+  # INFO: performs a create request to Coolpay API
+  #       and returns resource with Coolpay ID
+  class Create
+    def initialize(token, recipient_name)
+      @token          = token
+      @recipient_name = recipient_name
+    end
+
+    def call
+      JSON.parse(craete_recipient_response.body)
+    end
+
+    private
+
+    def craete_recipient_response
+      HTTParty.post(
+        CoolpayClient.recipients_url,
+        body:    body.to_json,
+        headers: headers
+      )
+    end
+
+    def body
+      { recipient: { name: @recipient_name } }
+    end
+
+    def headers
+      {
+        'Content-Type' => 'application/json',
+        'Authorization' => "Bearer #{@token}"
+      }
+    end
+  end
+end
---
+++ spec/services/api/recipients/create_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+describe API::Recipients::Create do
+  subject { described_class.new(token, recipient_name) }
+
+  let(:token)          { '12345' }
+  let(:recipient_name) { 'New Recipient'}
+  let(:headers) do
+    { 'Content-Type' => 'application/json', 'Authorization' => "Bearer #{token}" }
+  end
+  let(:body)           { { recipient: { name: recipient_name } } }
+  let(:recipient_hash) do
+    { 'recipient' => { 'id' => '123', 'name' => recipient_name } }
+  end
+  let(:request_params) do
+    [CoolpayClient.recipients_url, body: body.to_json, headers: headers]
+  end
+  let(:httparty_response) do
+    instance_double('HTTParty::Response', body: recipient_hash.to_json)
+  end
+
+  before do
+    allow(HTTParty)
+      .to receive(:post)
+      .with(*request_params)
+      .and_return(httparty_response)
+  end
+
+  it 'performs a post request to coolpay api recipients url' do
+    expect(HTTParty).to receive(:post).with(*request_params)
+    subject.call
+  end
+
+  it 'returns a token parsed from API response' do
+    expect(subject.call).to eq(recipient_hash)
+  end
+end
